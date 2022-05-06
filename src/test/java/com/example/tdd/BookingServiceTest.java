a40e2612994d731caa52d5a853242ff9efd1fea6
Daiane Feliciano <77757301+DaianeFeliciano@users.noreply.github.com>
2022-05-06 14:58:33 -0300

    teste

--- src/test/java/com/example/tdd/BookingServiceTest.java
+++ src/test/java/com/example/tdd/BookingServiceTest.java
@@ -1,24 +1,61 @@
 package com.example.tdd;
+import com.example.tdd.model.BookingModel;
+import com.example.tdd.repository.BookingRepository;
+import com.example.tdd.service.BookingService;
+import org.junit.Before;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class) //  Essa anotação faz intermédio entre os recursos do spring boot e o JUNiT
+import java.time.LocalDate;
+
+@SpringBootTest//  Essa anotação faz intermédio entre os recursos do spring boot e o JUNiT
+@RunWith(SpringRunner.class)
 public class BookingServiceTest {
 
     /*CICLO DO TDD
     * - ESCREVER OS TESTES QIE IRÃO FALHAR;
-    * - ARRUMAR OS TESTES
+    * - AJUSTES
     * - FAZ O REFACTORY*/
+
+    @TestConfiguration
+    static class BookingServiceTesteConfiguration {
+        @Bean
+        public BookingService bookingService() {
+            return new BookingService(); //Cria um Bean de BookingService/ p/ simular a instancia de booking service
+        }
+    }
 
     @Autowired
     BookingService bookingService;
 
+    @MockBean
+    BookingRepository bookingRepository;
+
     @Test //  vai calcular os dias da reserva
     public void bookingTesteServiceDaysCalculator () {
         String name = "Daiane";
-        int days = bookingService.daysCalculatorWithDataBase(name);
+        int days = bookingService.daysCalculatorWithDataBase(name); // retorna os dias da reversa
+
+        Assertions.assertEquals(days,10); // os dias e o esperado
+    }
+
+    @Before
+    public void setup() {
+
+        LocalDate checkIn = LocalDate.parse("2022-05-06");
+        LocalDate checkOut = LocalDate.parse("2022-05-16");
+        BookingModel bookingModel= new BookingModel("1", "Daiane", checkIn, checkOut, 2);
+
+        Mockito.when(bookingRepository.findByReserveName(bookingModel.getReserveName()))
+                .thenReturn(java.util.Optional.of(bookingModel));
     }
 
 
